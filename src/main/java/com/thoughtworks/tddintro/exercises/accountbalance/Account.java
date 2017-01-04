85c7782d0811d3cd000c5e9d856d8f6abc099908
MsLarissaaa <larissapittenger@gmail.com>
2017-01-04 01:29:20 -0600

    First test passing.

--- src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
@@ -1,4 +1,14 @@
 package com.thoughtworks.tddintro.exercises.accountbalance;
 
 public class Account {
+    int balance;
+
+    Account(int money){
+        balance = money;
+    }
+
+    public Integer deposit(int amount){
+        balance += amount;
+        return balance;
+    }
 }
--- src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
+++ src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
@@ -2,13 +2,14 @@ package com.thoughtworks.tddintro.exercises.accountbalance;
 
 import org.junit.Ignore;
 import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 public class AccountTests {
+    private final Account account = new Account(100);
+
     @Test
-    @Ignore  // Remove each @Ignore and implement test
-    public void shouldIncreaseMyBalanceWhenIDepositMoney(){
-        Account account = new Account();
-    }
+    public void shouldIncreaseMyBalanceWhenIDepositMoney(){assertThat(account.deposit(50), is(150));}
 
     @Test
     @Ignore  // Remove each @Ignore and implement test
