2dcdf6f5f35f73afa759ea23d6e044a0a379f78e
MsLarissaaa <larissapittenger@gmail.com>
2017-01-04 01:09:31 -0600

    All 5 tests passing.

--- src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
@@ -1,10 +1,14 @@
 package com.thoughtworks.tddintro.exercises.factorial;
 
 public class Factorial {
-    public Integer compute(int i) {
+    public Integer compute(int i) throws IllegalArgumentException {
         int fact = 1;
-        for(int k = 1; k <= i; k++){
-            fact *= k;
+        if(i < 0){
+            throw new IllegalArgumentException("Threw an IllegalArgumentException");
+        } else {
+            for(int k = 1; k <= i; k++){
+                fact *= k;
+            }
         }
         return fact;
     }
